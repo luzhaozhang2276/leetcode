94c15543e6e63c95539c03c683cb04e978d628e6
luzhaozhang2276 <luzhaozhang@doxmail.com>
2020-10-14 20:02:14 +0800

    剑指offer 06.从尾到头打印链表

--- main.cpp
+++ main.cpp
@@ -2,75 +2,109 @@
 
 using namespace std;
 
-class Solution {
-public:
-    string replaceSpace(string s) {
-        int numOfSpace = 0;
-        for (int i=0; i<s.length(); ++i)
-        {
-            if(s[i] == ' ')
-                ++numOfSpace;
-        }
-        cout << "num: " << numOfSpace << endl;
-        int p1 = s.size();
-        int newLength = s.length() + 2*numOfSpace;
-        s.resize(newLength);
-        while (p1>=0 && newLength>p1)
-        {
-            if(s[p1]==' ')
-            {
-                s[newLength--] = '0';
-                s[newLength--] = '2';
-                s[newLength--] = '%';
-            } else
-                s[newLength--] = s[p1];
-            --p1;
-        }
-
-        return s;
-    }
-};
-
 /**
- * @brief 注意知识点 string
- * 1. resize()
- * 2. reserve()
- * 3. capacity()
- * 4. size()
- * 5. length()
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
  */
 
-//class Solution {
-//public:
-//    string replaceSpace(string s) {
-//        int count = 0, len = s.size();
-//        // 统计空格数量
-//        for (char c : s) {
-//            if (c == ' ') count++;
-//        }
-//        // 修改 s 长度
-//        s.resize(len + 2 * count);
-//        // 倒序遍历修改
-//        for(int i = len - 1, j = s.size() - 1; i < j; i--, j--) {
-//            if (s[i] != ' ')
-//                s[j] = s[i];
-//            else {
-//                s[j - 2] = '%';
-//                s[j - 1] = '2';
-//                s[j] = '0';
-//                j -= 2;
-//            }
-//        }
-//        return s;
-//    }
-//};
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}     // 构造函数
+};
 
+class Solution {
+public:
+    std::vector<int> res;
+    vector<int> reversePrint(ListNode* head) {
 
+#if 1   // 入栈法
+        // std::stack<ListNode*> nodes;     // 只需要保存val即可,即stack<int>
+        stack<int> values;
+        while (head)
+        {
+            values.push(head->val);
+            head = head->next;
+        }
+
+        while(!values.empty())
+        {
+            res.push_back(values.top());
+            values.pop();
+        }
+#elif 0 // reverse法
+        while (head)
+        {
+            res.push_back(head->val);
+            head = head->next;
+        }
+        reverse(res.begin(), res.end());
+#elif 0 // 递归法
+        if(!head)
+            return res;
+        reversePrint(head->next);
+        res.push_back(head->val);
+        return res;
+#elif 0 // 改变链表结构法
+        ListNode *pre = nullptr;
+        ListNode *cur = head;
+        ListNode *next = head;
+        while (cur)
+        {
+            next = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = next;
+        }
+        while (pre)
+        {
+            res.push_back(pre->val);
+            pre = pre->next;
+        }
+#endif
+        return res;
+    }
+};
+/**
+ * @brief 4种方法:reverse反转法,堆栈法,递归法,改变链表结构法
+ * @note
+ */
 int main() {
-    string str = "We are happy.";
+    // 头插法
+    vector<int> init{2,3,1};
+    ListNode* head = (ListNode*)malloc(sizeof(ListNode));
+    head->next = nullptr;
+    for(auto ptr:init)
+    {
+        ListNode *p = (ListNode*)malloc(sizeof(ListNode));
+        p->val = ptr;
+        p->next = nullptr;
+        if(!head->next)
+            head->next = p;
+        else
+        {
+            p->next = head->next;
+            head->next = p;
+        }
+    }
 
+    cout << "print init list" << endl;
+    ListNode* p = head;
+    while(p->next)
+    {
+        cout << p->next->val << endl;
+        p = p->next;
+    }
+
+    cout << "\nprint new list" << endl;
     Solution s;
-    cout << s.replaceSpace(str) << endl;
+    vector<int> result(s.reversePrint(head->next));     /// leetcode 中,单链表的头指针对应的是头结点
+    for(auto ptr:result)
+        cout << ptr << endl;
 
     cout << "\nFinish" << endl;
     return 0;
