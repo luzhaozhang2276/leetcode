563cce7182fb50a80ffd01762baec2ee903a1228
lu <luzhaozhang@foxmail.com>
2020-11-01 16:02:48 +0800

    基于partition的快速排序算法

--- main.cpp
+++ main.cpp
@@ -1,32 +1,67 @@
 #include <bits/stdc++.h>
+//#include <random>
+//#include <chrono>
 
 using namespace std;
 
-class Solution {
-public:
-    int numWays(int n) {
-        if (n < 2)
-            return 1;
+void inline swap(int &x, int &y)
+{
+    int t = x;
+    x = y;
+    y = t;
+}
 
-        int fibNMinusOne(1);
-        int finBMinusTwo(1);
-        int fibN(0);
+int inline randRange(int low, int high)
+{
+    uniform_int_distribution<int> u(low, high);
+    mt19937 rand_generator(chrono::high_resolution_clock::now().time_since_epoch().count());
+    return u(rand_generator);
+}
 
-        while (n>=2)
+int partition(int nums[], int low, int high)
+{
+    int id = randRange(low, high);
+    swap(nums[id], nums[high]);
+
+    int i=0,j=0;
+    for (i = low, j = low; j < high; ++j)
+    {
+        if (nums[j] <= nums[high])
         {
-            fibN = (fibNMinusOne + finBMinusTwo)%1000000007;
-            finBMinusTwo = fibNMinusOne;
-            fibNMinusOne = fibN;
-            --n;
+                swap(nums[i++], nums[j]);
         }
-
-        return fibN;
     }
-};
+    swap(nums[i], nums[j]);
+    return i;
+}
+
+void quickSort(int nums[], int begin, int end)
+{
+
+    if (begin >= end) return;
+
+    int index = partition(nums, begin, end);
+    if (index>begin)
+        quickSort(nums, begin, index-1);
+    if (index<end)
+        quickSort(nums, index+1, end);
+}
+
 
 int main() {
-    Solution s;
-    cout << s.numWays(7) << endl;
+    int data[] = {5,9,1,2,8,6,3,7};
+//    //int data[] = {5,9,2,1,4,7,5,8,3,6};
+//
+    for (auto p:data)
+        cout << p << ' ';
+    cout << endl;
+
+    quickSort(data, 0, 7);
+
+    cout << endl;
+    for (auto p:data)
+        cout << p << ' ';
+    cout << endl;
 
     cout << "\nFinish" << endl;
     return 0;
