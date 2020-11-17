ed943e2116a93792a3801d96bca9cf44ce5dd421
lu <luzhaozhang@foxmail.com>
2020-11-17 22:00:21 +0800

    剑指 Offer 11. 旋转数组的最小数字

--- main.cpp
+++ main.cpp
@@ -1,67 +1,37 @@
 #include <bits/stdc++.h>
-//#include <random>
-//#include <chrono>
-
 using namespace std;
 
-void inline swap(int &x, int &y)
-{
-    int t = x;
-    x = y;
-    y = t;
-}
-
-int inline randRange(int low, int high)
-{
-    uniform_int_distribution<int> u(low, high);
-    mt19937 rand_generator(chrono::high_resolution_clock::now().time_since_epoch().count());
-    return u(rand_generator);
-}
-
-int partition(int nums[], int low, int high)
-{
-    int id = randRange(low, high);
-    swap(nums[id], nums[high]);
-
-    int i=0,j=0;
-    for (i = low, j = low; j < high; ++j)
-    {
-        if (nums[j] <= nums[high])
-        {
-                swap(nums[i++], nums[j]);
+/// 不必像书中那样重新顺序查找
+class Solution {
+public:
+    int minArray(vector<int>& numbers) {
+        int low = 0;
+        int high = numbers.size() - 1;
+        while (low < high) {
+            int pivot = low + (high - low) / 2;
+            if (numbers[pivot] < numbers[high]) {
+                high = pivot;
+            }
+            else if (numbers[pivot] > numbers[high]) {
+                low = pivot + 1;
+            }
+            else {
+                high -= 1;
+            }
         }
+        return numbers[low];
     }
-    swap(nums[i], nums[j]);
-    return i;
-}
-
-void quickSort(int nums[], int begin, int end)
-{
-
-    if (begin >= end) return;
-
-    int index = partition(nums, begin, end);
-    if (index>begin)
-        quickSort(nums, begin, index-1);
-    if (index<end)
-        quickSort(nums, index+1, end);
-}
+};
 
 
 int main() {
-    int data[] = {5,9,1,2,8,6,3,7};
-//    //int data[] = {5,9,2,1,4,7,5,8,3,6};
-//
-    for (auto p:data)
-        cout << p << ' ';
-    cout << endl;
-
-    quickSort(data, 0, 7);
-
-    cout << endl;
-    for (auto p:data)
-        cout << p << ' ';
-    cout << endl;
+//    vector<int> nums{3,4,5,1,2};
+//    vector<int> nums{2,2,2,0,1};
+//    vector<int> nums{1,2,3,4,5};
+//    vector<int> nums{10,1,10,10,10};
+    vector<int> nums{10,10,10,1,10};
+    Solution s;
+    cout << "min: " << s.minArray(nums) << endl;
 
     cout << "\nFinish" << endl;
     return 0;
