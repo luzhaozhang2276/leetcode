cf6889e5c61fb97d4b35aac82748f692417cc837
lu <luzhaozhang@foxmail.com>
2020-11-25 22:01:51 +0800

    剑指 Offer 14- I. 剪绳子

--- main.cpp
+++ main.cpp
@@ -1,103 +1,67 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/*
+#if 1       // 动态规划问题
 class Solution {
 public:
-    bool exist(vector<vector<char>>& board, const string& word) {
-        //cout << board.size() << "*" << board[0].size() << " , " << word.size() << endl;
-        rows = board.size();
-        cols = board[0].size();
-        for (int row = 0; row < rows; ++row)
+    int cuttingRope(int n) {
+        if (n < 2)
+            return 0;
+        if (n == 2)
+            return 1;
+        if (n == 3)
+            return 2;
+
+        int* ans = new int[n+1];
+        ans[0] = 0;
+        ans[1] = 1;
+        ans[2] = 2;
+        ans[3] = 3;
+
+        int max;
+        for (int i = 4; i <= n; ++i)
         {
-            for (int col = 0; col < cols; ++col) {
-                if (dfs(board, word, row, col, 0))
-                    return true;
+            max = 0;
+            for (int j = 1; j <= i/2; ++j)
+            {
+                max = (ans[j]*ans[i-j] > max) ? ans[j]*ans[i-j] : max;
+                ans[i] = max;
             }
         }
+        max = ans[n];
+        delete[] ans;
 
-        return false;
-    }
-
-private:
-    int rows;
-    int cols;
-    bool dfs(vector<vector<char>>& board, const string& word, int r, int c, int k)
-    {
-        if (r < 0 || r >= rows || c < 0 || c >= cols || board[r][c] != word[k])
-            return false;
-        if (k == word.size()-1)
-            return true;
-
-
-        board[r][c] = '\0';
-        bool res = dfs(board, word, r-1, c, k+1) || dfs(board, word, r+1, c, k+1)
-                || dfs(board, word, r, c-1, k+1) || dfs(board, word, r, c+1, k+1);
-
-        board[r][c] = word[k];
-
-//        if(res)
-//            cout << board[r][c] << endl;
-        return res;
+        return max;
     }
 };
-//    vector<vector<char>> board = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
-//    vector<vector<char>> board = {{'A','B','C'},{'S','F','C'},{'A','D','E'}};
-//    string word = "ABCCED";
-//    string word = "ABCCFSAD";
-//    word = '\0';
 
-//    Solution s;
-//    cout << s.exist(board, word);
-
-*/
+#else   // 贪婪算法
 
 class Solution {
 public:
-    int rows;
-    int cols;
-    int k_;
-    int count = 0;
-    bool vis[100][100] = {};
+    int cuttingRope(int n) {
+        if (n < 2)
+            return 0;
+        if (n == 2)
+            return 1;
+        if (n == 3)
+            return 2;
 
-    int movingCount(int m, int n, int k) {
-        rows = m;
-        cols = n;
-        k_ = k;
+        int time3 = n/3;
+        if ((n - 3*time3) == 1)
+            time3--;
 
-        dfs(0, 0);
-        return count;
-    }
+        int time2 = (n - 3*time3) / 2;
 
-    static int getsum(int num)
-    {
-        int sum = 0;
-        while (num > 0)
-        {
-            sum += num % 10;
-            num /= 10;
-        }
-        return sum;
-    }
-
-    bool dfs(int r, int c)
-    {
-        if (r < 0 || r >= rows || c < 0 || c >= cols || (getsum(r) + getsum(c) > k_) || vis[r][c])
-            return false;
-
-//        cout << "[" << r << "," << c << "]" << endl;
-
-        vis[r][c] = true;
-        count++;
-
-        dfs(r-1, c) + dfs(r+1, c) + dfs(r, c-1) + dfs(r, c+1);
-        return true;
+        return int(pow(3,time3) * pow(2, time2));
     }
 };
+
+#endif
 
 int main() {
     Solution s;
-    cout << s.movingCount(3,2,17) << endl;
+    cout << "max: " << s.cuttingRope(10) << endl;
 
 
     cout << "\nFinish" << endl;
