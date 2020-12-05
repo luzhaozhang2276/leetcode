e606af3c1444b9b7a8302891bbd9c7577c4b4cea
lu <luzhaozhang@foxmail.com>
2020-12-05 16:54:41 +0800

    剑指 Offer 16. 数值的整数次方

--- main.cpp
+++ main.cpp
@@ -1,60 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#if 0
 class Solution {
 public:
-    int hammingWeight(uint32_t n) {
-        int count = 0;
-        for (int i = 0; i < 32; ++i)
+    double myPow(double x, long int n) {
+        double base = 1;
+        int plus_n = 1;
+        n = floor(n);
+        if (n == 0)
+            return 1;
+        if (n < 0)
         {
-            if (n & 1)
-                count++;
-            n = n >> 1;
+            // if (x == 0)
+            //     return 0;
+            plus_n = -1;
         }
-        return count;
+
+        /// 循环求幂  超出时间限制
+        // for (int i = 0; i < plus_n * n; ++i)
+        //     base *= x;
+
+        /// 二分求幂
+        base = computePow(x,plus_n * n);
+
+        return plus_n == 1 ? base : 1/base;
+    }
+
+    double computePow(double x, long int e) {
+        double base = 1;
+        if (e == 1)
+            return x;
+        base = computePow(x, e>>1); // 右移除2
+        if ((e & 0x1) == 1)     // 判断奇偶
+            return x * base*base;
+        return base*base;
     }
 };
-
-#elif 0
-
-class Solution {
-public:
-    int hammingWeight(uint32_t n) {
-        int count = 0;
-        unsigned int flag = 1;
-        while (flag)
-        {
-            if (n & flag)
-                count++;
-            flag = flag << 1;
-        }
-        return count;
-    }
-};
-
-#elif 1
-
-class Solution {
-public:
-    int hammingWeight(uint32_t n) {
-        int count = 0;
-        while (n)
-        {
-            ++count;
-            n = (n-1) & n;
-        }
-        return count;
-    }
-};
-
-#endif
 
 int main() {
     Solution s;
-    uint32_t t = 00000000000000000000000000001011;
-    cout << "counts: " << s.hammingWeight(t) << endl;
-
+    cout << "result = " << s.myPow(1,-2147483648) << endl;
 
     cout << "\nFinish" << endl;
     return 0;
