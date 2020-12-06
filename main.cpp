9315b9edbbc438a4b9ca60c99efe350e86128fd8
lu <luzhaozhang@foxmail.com>
2020-12-06 15:23:01 +0800

    剑指 Offer 17. 打印从1到最大的n位数

--- main.cpp
+++ main.cpp
@@ -1,45 +1,151 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#if 0
+/// 简单解法(不考虑大数问题)
 class Solution {
 public:
-    double myPow(double x, long int n) {
-        double base = 1;
-        int plus_n = 1;
-        n = floor(n);
-        if (n == 0)
-            return 1;
-        if (n < 0)
-        {
-            // if (x == 0)
-            //     return 0;
-            plus_n = -1;
-        }
+    vector<int> res;
+    vector<int> printNumbers(int n) {
+        int number = pow(10,n);
+        for (int i = 1; i < number; ++i)
+            res.push_back(i);
 
-        /// 循环求幂  超出时间限制
-        // for (int i = 0; i < plus_n * n; ++i)
-        //     base *= x;
-
-        /// 二分求幂
-        base = computePow(x,plus_n * n);
-
-        return plus_n == 1 ? base : 1/base;
-    }
-
-    double computePow(double x, long int e) {
-        double base = 1;
-        if (e == 1)
-            return x;
-        base = computePow(x, e>>1); // 右移除2
-        if ((e & 0x1) == 1)     // 判断奇偶
-            return x * base*base;
-        return base*base;
+        return res;
     }
 };
 
+#elif 0
+/// 考虑大数问题,char模拟加法运算(不使用递归)
+class Solution {
+public:
+    vector<int> res;
+    vector<int> printNumbers(int n) {
+        if (n <= 0)
+            return res;
+        char* number = new char[n+1];
+        memset(number, '0', n);
+        number[n] = '\0';
+
+        while (!Increment(number))
+            Save(number);
+
+        delete []number;
+        return res;
+    }
+
+    bool Increment(char *number) {
+        bool isOverflow = false;    // 越界
+        int nTakeOver = 0;      // 进位
+        int nLength = strlen(number);
+
+        for (int i = nLength - 1; i >= 0; --i) {
+            int nSum = number[i] - '0' + nTakeOver;
+
+            if (i == nLength - 1)
+                nSum++;
+
+            if (nSum >= 10) {
+                if (i == 0)
+                    isOverflow = true;
+                else {
+                    nSum -= 10;
+                    nTakeOver = 1;
+                    number[i] = '0' + nSum;
+                }
+            }
+            else {
+                number[i] = '0' + nSum;
+                break;
+            }
+        }
+
+        return isOverflow;
+    }
+
+    void Save(char *number) {
+        string s = "";
+        bool isBeginning0 = true;
+        int nLength = strlen(number);
+        for (int i = 0; i < nLength; ++i)
+        {
+            if (isBeginning0 && number[i] != '0')
+                isBeginning0 = false;
+            if (!isBeginning0)
+                s += number[i];
+        }
+
+        int num = stoi(s);
+        res.push_back(num);
+    }
+};
+
+#elif 1
+/// 全排列问题 (递归)
+class Solution {
+public:
+    vector<int> res;
+    vector<int> printNumbers(int n) {
+        if (n <= 0)
+            return res;
+        char* number = new char[n+1];
+        memset(number, '0', n);
+        number[n] = '\0';
+
+        for (size_t i = 0; i < 10; ++i)
+        {
+            number[0] = i + '0';
+            ComputeRecursion(number, n, 0);
+        }
+
+        delete []number;
+        return res;
+    }
+
+    void ComputeRecursion(char* number, int length, int index)
+    {
+        if (index == length -1)
+        {
+            Save(number);
+            return;
+        }
+
+        for (size_t i = 0; i < 10; ++i)
+        {
+            number[index+1] = i + '0';
+            ComputeRecursion(number, length, index+1);
+        }
+    }
+
+    void Save(char *number) {
+        string s = "";
+        bool isBeginning0 = true;
+        int nLength = strlen(number);
+        for (int i = 0; i < nLength; ++i)
+        {
+            if (isBeginning0 && number[i] != '0')
+                isBeginning0 = false;
+            if (!isBeginning0)
+                s += number[i];
+        }
+
+        if (!isBeginning0)
+        {
+            int num = stoi(s);
+            res.push_back(num);
+        }
+    }
+};
+
+#endif
+
+
+
 int main() {
     Solution s;
-    cout << "result = " << s.myPow(1,-2147483648) << endl;
+    cout << "print:" << endl;
+    for (const auto &p:s.printNumbers(2))
+        cout << p << endl;
 
     cout << "\nFinish" << endl;
     return 0;
