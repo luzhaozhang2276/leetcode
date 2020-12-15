2fb567e291637a995e6cd9d3549ad20fec9d09ac
lu <luzhaozhang@foxmail.com>
2020-12-15 15:15:41 +0800

    剑指 Offer 21. 调整数组顺序使奇数位于偶数前面

--- main.cpp
+++ main.cpp
@@ -2,186 +2,78 @@
 using namespace std;
 
 #if 0
-/// 常规解法
+/// 双指针
 class Solution {
 public:
-    string::iterator ite;
-    string::iterator end;
-
-    bool scanUnsignedInteger()
-    {
-        const auto before = ite;
-        while (ite != end && *ite >= '0' && *ite <= '9')
-            ++ite;
-
-        return ite > before;
+    vector<int> exchange(vector<int>& nums) {
+        if (nums.empty())
+            return nums;
+        return reorder(nums, isEven);
     }
 
-    bool scanInteger()
+    vector<int> reorder(vector<int>& nums, bool (*func)(int))
     {
-        if (*ite == '+' || *ite == '-')
-            ++ite;
+        auto begin = nums.begin();
+        auto end = nums.end()-1;
 
-        return scanUnsignedInteger();
+        while (begin <= end)
+        {
+            while (begin < end && !func(*begin))
+                begin++;
+
+            while (begin < end && func(*end))
+                end--;
+
+            if (begin < end)
+            {
+                int temp = *begin;
+                *begin = *end;
+                *end = temp;
+            }
+        }
+        return nums;
     }
 
-
-    bool isNumber(string s) {
-        s.erase(0, s.find_first_not_of(" "));   // s.find_first_not_of(" ") 查找第一个不是空格的位置
-        s.erase(s.find_last_not_of(" ")+1);       // s.find_last_not_of(" ") 查找最后一个不是空格的位置
-        if (s.empty())
-            return false;
-
-        ite = s.begin();
-        end = s.end();
-
-        if (ite >= end)
-            return false;
-
-        bool numeric = scanInteger();
-
-        if (*ite == '.')
-        {
-            ++ite;
-            numeric = scanUnsignedInteger() || numeric;
-        }
-
-        if (*ite == 'e' || *ite == 'E')
-        {
-            ++ite;
-            numeric = numeric && scanInteger();
-        }
-
-        return numeric && ite == end;
+    static bool isEven(int n)
+    {
+        return (n & 0x1) == 0;
     }
 };
 
 #elif 1
-
-/// 确定有限状态自动机
+/// 快慢双指针
 class Solution {
 public:
-    enum State {
-        STATE_INITIAL,
-        STATE_INT_SIGN,
-        STATE_INTEGER,
-        STATE_POINT,
-        STATE_POINT_WITHOUT_INT,
-        STATE_FRACTION,
-        STATE_EXP,
-        STATE_EXP_SIGN,
-        STATE_EXP_NUMBER,
-        STATE_END,
-    };
+    vector<int> exchange(vector<int>& nums) {
+        auto fast = nums.begin();
+        auto slow = nums.begin();
 
-    enum CharType {
-        CHAR_NUMBER,
-        CHAR_EXP,
-        CHAR_POINT,
-        CHAR_SIGN,
-        CHAR_SPACE,
-        CHAR_ILLEGAL,
-    };
-
-    CharType toCharType(char ch) {
-        if (ch >= '0' && ch <= '9') {
-            return CHAR_NUMBER;
-        } else if (ch == 'e' || ch == 'E') {
-            return CHAR_EXP;
-        } else if (ch == '.') {
-            return CHAR_POINT;
-        } else if (ch == '+' || ch == '-') {
-            return CHAR_SIGN;
-        } else if (ch == ' ') {
-            return CHAR_SPACE;
-        } else {
-            return CHAR_ILLEGAL;
-        }
-    }
-
-    bool isNumber(string s) {
-        unordered_map<State, unordered_map<CharType, State>> transfer{
+        while (fast != nums.end())
+        {
+            if (*fast & 1)  // odd
             {
-                STATE_INITIAL, {
-                    {CHAR_SPACE, STATE_INITIAL},
-                    {CHAR_NUMBER, STATE_INTEGER},
-                    {CHAR_POINT, STATE_POINT_WITHOUT_INT},
-                    {CHAR_SIGN, STATE_INT_SIGN},
-                }
-            }, {
-                STATE_INT_SIGN, {
-                    {CHAR_NUMBER, STATE_INTEGER},
-                    {CHAR_POINT, STATE_POINT_WITHOUT_INT},
-                }
-            }, {
-                STATE_INTEGER, {
-                    {CHAR_NUMBER, STATE_INTEGER},
-                    {CHAR_EXP, STATE_EXP},
-                    {CHAR_POINT, STATE_POINT},
-                    {CHAR_SPACE, STATE_END},
-                }
-            }, {
-                STATE_POINT, {
-                    {CHAR_NUMBER, STATE_FRACTION},
-                    {CHAR_EXP, STATE_EXP},
-                    {CHAR_SPACE, STATE_END},
-                }
-            }, {
-                STATE_POINT_WITHOUT_INT, {
-                    {CHAR_NUMBER, STATE_FRACTION},
-                }
-            }, {
-                STATE_FRACTION,
-                {
-                    {CHAR_NUMBER, STATE_FRACTION},
-                    {CHAR_EXP, STATE_EXP},
-                    {CHAR_SPACE, STATE_END},
-                }
-            }, {
-                STATE_EXP,
-                {
-                    {CHAR_NUMBER, STATE_EXP_NUMBER},
-                    {CHAR_SIGN, STATE_EXP_SIGN},
-                }
-            }, {
-                STATE_EXP_SIGN, {
-                    {CHAR_NUMBER, STATE_EXP_NUMBER},
-                }
-            }, {
-                STATE_EXP_NUMBER, {
-                    {CHAR_NUMBER, STATE_EXP_NUMBER},
-                    {CHAR_SPACE, STATE_END},
-                }
-            }, {
-                STATE_END, {
-                    {CHAR_SPACE, STATE_END},
-                }
+                swap(*fast, *slow);
+                slow++;
             }
-        };
-
-        int len = s.length();
-        State st = STATE_INITIAL;
-
-        for (int i = 0; i < len; i++) {
-            CharType typ = toCharType(s[i]);
-            if (transfer[st].find(typ) == transfer[st].end()) {
-                return false;
-            } else {
-                st = transfer[st][typ];
-            }
+            fast++;
         }
-        return st == STATE_INTEGER || st == STATE_POINT || st == STATE_FRACTION || st == STATE_EXP_NUMBER || st == STATE_END;
+        return nums;
     }
 };
 
 #endif
 
+
 int main() {
-    string s = "1 ";
+    vector<int> nums = {1,2,4,3};
+    for (const auto &p:nums)
+        cout << p << " ";
+    cout << endl;
 
     Solution solve;
-    string result = solve.isNumber(s) ? "true" : "false";
-    cout << "result: " << result << endl;
+    for (auto &p:solve.exchange(nums))
+        cout << p << " ";
+
 
     cout << "\nFinish" << endl;
     return 0;
