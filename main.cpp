bf6c9d6e4feedc86c13982dd3b2aa4bc682af09d
lu <luzhaozhang@foxmail.com>
2020-12-15 20:24:21 +0800

    剑指 Offer 22. 链表中倒数第k个节点

--- main.cpp
+++ main.cpp
@@ -1,79 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#if 0
-/// 双指针
-class Solution {
-public:
-    vector<int> exchange(vector<int>& nums) {
-        if (nums.empty())
-            return nums;
-        return reorder(nums, isEven);
-    }
 
-    vector<int> reorder(vector<int>& nums, bool (*func)(int))
-    {
-        auto begin = nums.begin();
-        auto end = nums.end()-1;
-
-        while (begin <= end)
-        {
-            while (begin < end && !func(*begin))
-                begin++;
-
-            while (begin < end && func(*end))
-                end--;
-
-            if (begin < end)
-            {
-                int temp = *begin;
-                *begin = *end;
-                *end = temp;
-            }
-        }
-        return nums;
-    }
-
-    static bool isEven(int n)
-    {
-        return (n & 0x1) == 0;
-    }
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
 };
 
-#elif 1
-/// 快慢双指针
 class Solution {
 public:
-    vector<int> exchange(vector<int>& nums) {
-        auto fast = nums.begin();
-        auto slow = nums.begin();
+    ListNode* getKthFromEnd(ListNode* head, int k) {
+        if (head == nullptr || k == 0)
+            return nullptr;
 
-        while (fast != nums.end())
+        ListNode *pAhead = head;
+        ListNode *pBehind = head;
+
+        for (int i = 0; i < k - 1; ++i)
         {
-            if (*fast & 1)  // odd
-            {
-                swap(*fast, *slow);
-                slow++;
-            }
-            fast++;
+            pAhead = pAhead->next;
+            if (pAhead == nullptr)
+                return nullptr;
         }
-        return nums;
+
+        while (pAhead->next)
+        {
+            pAhead = pAhead->next;
+            pBehind = pBehind->next;
+        }
+
+        return pBehind;
     }
 };
-
-#endif
 
 
 int main() {
-    vector<int> nums = {1,2,4,3};
-    for (const auto &p:nums)
-        cout << p << " ";
-    cout << endl;
+    vector<int> number = {1,2,3,4,5};
+
+    auto *list = new ListNode(0);
+    ListNode *ptr = list;
+    for (auto &p:number)
+    {
+        ptr->next = new ListNode(p);
+        ptr = ptr->next;
+    }
+
+    ptr = list->next;
+    delete list;
 
     Solution solve;
-    for (auto &p:solve.exchange(nums))
-        cout << p << " ";
 
+    auto p = solve.getKthFromEnd(ptr,2);
+    while (p)
+    {
+        cout << p->val << " ";
+        p = p->next;
+    }
 
     cout << "\nFinish" << endl;
     return 0;
