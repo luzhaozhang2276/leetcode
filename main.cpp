8b63d7d0bece8de2b736cc7377ee80669db4d287
lu <luzhaozhang@foxmail.com>
2020-12-26 10:54:12 +0800

    剑指 Offer 29. 顺时针打印矩阵

--- main.cpp
+++ main.cpp
@@ -1,82 +1,97 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
-#if 1
+#if 0
 class Solution {
 public:
-    bool isSymmetric(TreeNode* root) {
-        return recurtion(root, root);
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        if (matrix.empty() || matrix[0].empty())
+            return res;
+
+        int start = 0;
+        while (matrix.size() > 2*start && matrix[0].size() > 2*start)
+        {
+            int endX = matrix[0].size() - 1 - start;
+            int endY = matrix.size() -1 -start;
+
+            // 左右
+            for (int i = start; i <= endX; ++i)
+                res.push_back(matrix[start][i]);
+            // 上下
+            if (start < endY) {
+                for (int i = start + 1; i <= endY; ++i)
+                    res.push_back(matrix[i][endX]);
+            }
+            // 右左
+            if (start < endX && start < endY){
+                for (int i = endX-1; i >= start; --i)
+                    res.push_back(matrix[endY][i]);
+            }
+            //下上
+            if (start < endX && start < endY - 1) {
+                for (int i = endY - 1; i >= start + 1; --i)
+                    res.push_back(matrix[i][start]);
+            }
+
+            ++start;
+        }
+
+        return res;
     }
 
-    bool recurtion(TreeNode* pRoot1, TreeNode* pRoot2)
-    {
-        if (pRoot1 == nullptr && pRoot2 == nullptr)
-            return true;
+private:
+    vector<int> res;
+};
 
-        if (pRoot1 == nullptr || pRoot2 == nullptr)
-            return false;
+#elif 1
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        if (matrix.empty() || matrix[0].empty())
+            return res;
 
-        if (pRoot1->val != pRoot2->val)
-            return false;
+        // 左右上下四个边界: l r t b
+        int l = 0, r = matrix[0].size() - 1, t = 0, b = matrix.size() - 1;
+        while (true)
+        {
+            // left to right
+            for (int i = l; i <= r; ++i)
+                res.push_back(matrix[t][i]);
+            if (++t > b)
+                break;
+            // top to bottom
+            for (int i = t; i <= b; ++i)
+                res.push_back(matrix[i][r]);
+            if (l > --r)
+                break;
+            // right to left
+            for (int i = r; i >= l; --i)
+                res.push_back(matrix[b][i]);
+            if (t > --b)
+                break;
+            // bottom to top
+            for (int i = b; i >= t; --i)
+                res.push_back(matrix[i][l]);
+            if (++l > r)
+                break;
+        }
 
-        return recurtion(pRoot1->left, pRoot2->right) && recurtion(pRoot1->right, pRoot2->left);
+        return res;
     }
+
+private:
+    vector<int> res;
 };
 #endif
 
-/// 生成二叉树: 使用队列(先进先出)
-TreeNode* createTree(vector<int> &number)
-{
-    if (number.empty())
-        return nullptr;
-
-    auto ptr = number.begin();
-
-    TreeNode* root = new TreeNode(*ptr);
-    queue<TreeNode*> nodeQueue;
-    nodeQueue.push(root);
-    while (ptr != number.end())
-    {
-        TreeNode* node = nodeQueue.front();
-        nodeQueue.pop();
-
-        ptr++;
-        if(ptr != number.end() && *ptr != '\0') {
-            node->left = new TreeNode(*ptr);
-            nodeQueue.push(node->left);
-        }
-
-        if(ptr == number.end())
-            break;
-
-        ptr++;
-        if(ptr != number.end() && *ptr != '\0')
-        {
-            node->right = new TreeNode(*ptr);
-            nodeQueue.push(node->right);
-        }
-    }
-
-    return root;
-}
-
 int main() {
     /// 数据生成 ptr
-    vector<int> A = {1,2,2,3,4,4,3};
-    TreeNode* treeA = createTree(A);
+    vector<vector<int> > matrix = {{1,2,3},{4,5,6},{7,8,9}};
 
     Solution solve;
-    string result = solve.isSymmetric(treeA) ? "True" : "False";
-    cout << "result: " << result << endl;
+
+    for (auto p:solve.spiralOrder(matrix))
+        cout << p << " ";
 
     cout << "\nFinish" << endl;
     return 0;
