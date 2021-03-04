37cd52b30afd7f26afc0d08c52fa9203866d9ba3
lu <luzhaozhang@foxmail.com>
2021-03-04 15:21:34 +0800

    剑指 Offer 52. 两个链表的第一个公共节点

--- main.cpp
+++ main.cpp
@@ -1,44 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 归并排序 递归
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
 class Solution {
 public:
-    int reversePairs(vector<int>& nums) {
-        vector<int> tmp(nums.size());
-        return mergeSort(0, nums.size() - 1, nums, tmp);
-    }
-
-private:
-    int mergeSort(int l, int r, vector<int>& nums, vector<int>& tmp) {
-        // 终止条件
-        if (l >= r)
-            return 0;
-        // 递归划分
-        int m = (l + r) / 2;    // 中点划分: i(起点,前半部), j(中点,后半部)
-        int res = mergeSort(l, m, nums, tmp) + mergeSort(m + 1, r, nums, tmp);
-        // 合并阶段
-        int i = l, j = m + 1;
-        for (int k = l; k <= r; ++k)    // tmp 复制需要排序的子片段,之后进行前后对比,将结果依次存到nums中
-            tmp[k] = nums[k];
-        for (int k = l; k <= r; ++k) {
-            if (i == m + 1)             // 前者已变遍历完,将剩余后者存入nums中
-                nums[k] = tmp[j++];
-            else if (j == r + 1 || tmp[i] <= tmp[j])    // 不存在逆序对,将前者(小)存入nums中
-                nums[k] = tmp[i++];
-            else {                      // 存在逆序对,计数更新,并将后者(小)存入nums中
-                nums[k] = tmp[j++];
-                res += m - i + 1;
-            }
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *A = headA, *B = headB;
+        while (A != B) {
+            A = A != nullptr ? A->next : headB;
+            B = B != nullptr ? B->next : headA;
         }
-        return res;
+        return A;
     }
 };
 
+void generateList(ListNode *headA, ListNode *headB) {
+    vector<int> A = {4, 1};
+    vector<int> B = {5, 0, 1, 8, 4, 5};
+
+    ListNode *ptr = headB, *pinter;
+    for (int p:B) {
+        ptr->next = new ListNode(p);
+        ptr = ptr->next;
+        if (p == 8)
+            pinter = ptr;
+    }
+
+    ptr = headA;
+    for (int p:A) {
+        ptr->next = new ListNode(p);
+        ptr = ptr->next;
+    }
+    ptr->next = pinter;
+
+}
+
+
 int main() {
-    vector<int> nums = {7,5,6,4};
+    auto *headA = new ListNode(0);
+    auto *headB = new ListNode(0);
+    generateList(headA, headB);
+    headA = headA->next;
+    headB = headB->next;
+
     Solution solve;
-    cout << "res = " << solve.reversePairs(nums) << endl;
+    cout << "node = " << solve.getIntersectionNode(headA, headB)->val << endl;
 
     cout << "\nFinish";
     return 0;
