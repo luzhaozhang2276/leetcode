0260e64e7aa57640ee652569dfada48f69076863
lu <luzhaozhang@foxmail.com>
2021-03-04 16:13:14 +0800

    剑指 Offer 53 - I. 在排序数组中查找数字 I

--- main.cpp
+++ main.cpp
@@ -1,55 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-
+#if 1
+// 二分法
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode *A = headA, *B = headB;
-        while (A != B) {
-            A = A != nullptr ? A->next : headB;
-            B = B != nullptr ? B->next : headA;
+    int search(vector<int>& nums, int target) {
+        return findright(nums, target) - findright(nums, target - 1);
+    }
+
+private:
+    int findright(vector<int>& nums, int target) {
+        // 找右边界
+        int l = 0, r = nums.size() - 1;
+        while (l <= r) {
+            int middle = (l + r) / 2;
+            if (nums[middle] <= target)
+                l = middle + 1;
+            else
+                r = middle - 1;
         }
-        return A;
+        return l;
     }
 };
+#elif 1
+// 二分法 (代码冗余)
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left, right;
+        // 找右边界
+        int l = 0, r = nums.size() - 1;
+        while (l <= r) {
+            int middle = (l + r) / 2;
+            if (nums[middle] <= target)
+                l = middle + 1;
+            else
+                r = middle - 1;
+        }
+        right = l;
 
-void generateList(ListNode *headA, ListNode *headB) {
-    vector<int> A = {4, 1};
-    vector<int> B = {5, 0, 1, 8, 4, 5};
+        // 找左边界
+        l = 0, r = nums.size() - 1;
+        while (l <= r) {
+            int middle = (l + r) / 2;
+            if (nums[middle] < target)
+                l = middle + 1;
+            else
+                r = middle - 1;
+        }
+        left = r;
 
-    ListNode *ptr = headB, *pinter;
-    for (int p:B) {
-        ptr->next = new ListNode(p);
-        ptr = ptr->next;
-        if (p == 8)
-            pinter = ptr;
+        return right - left - 1;
     }
-
-    ptr = headA;
-    for (int p:A) {
-        ptr->next = new ListNode(p);
-        ptr = ptr->next;
-    }
-    ptr->next = pinter;
-
-}
-
+};
+#endif
 
 int main() {
-    auto *headA = new ListNode(0);
-    auto *headB = new ListNode(0);
-    generateList(headA, headB);
-    headA = headA->next;
-    headB = headB->next;
+    vector<int> nums = {5,7,7,8,8,10};
 
     Solution solve;
-    cout << "node = " << solve.getIntersectionNode(headA, headB)->val << endl;
+    cout << "num = " << solve.search(nums, 8) << endl;
 
     cout << "\nFinish";
     return 0;
