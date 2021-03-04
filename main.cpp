32d1f34eb36bf4c2122da224bcde38226fa5ccc2
lu <luzhaozhang@foxmail.com>
2021-03-04 10:46:19 +0800

    剑指 Offer 51. 数组中的逆序对

--- main.cpp
+++ main.cpp
@@ -1,63 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#if 0
-/// 哈希表
+// 归并排序 递归
 class Solution {
 public:
-    char firstUniqChar(string s) {
-        unordered_map<char, bool> hashmap;
-        for (auto c:s)
-            hashmap[c] = hashmap.find(c) == hashmap.end();
-        for (auto c:s)
-            if (hashmap[c])
-                return c;
-        return ' ';
+    int reversePairs(vector<int>& nums) {
+        vector<int> tmp(nums.size());
+        return mergeSort(0, nums.size() - 1, nums, tmp);
     }
-};
 
-#elif 0
-/// 有序哈希表
-class Solution {
-public:
-    char firstUniqChar(string s) {
-        vector<char> keys;
-        unordered_map<char, bool> hashmap;
-        for (auto c:s) {
-            if (hashmap.find(c) == hashmap.end())   // 记录每个字符抽次出现的次序
-                keys.push_back(c);
-            hashmap[c] = hashmap.find(c) == hashmap.end();
-
+private:
+    int mergeSort(int l, int r, vector<int>& nums, vector<int>& tmp) {
+        // 终止条件
+        if (l >= r)
+            return 0;
+        // 递归划分
+        int m = (l + r) / 2;    // 中点划分: i(起点,前半部), j(中点,后半部)
+        int res = mergeSort(l, m, nums, tmp) + mergeSort(m + 1, r, nums, tmp);
+        // 合并阶段
+        int i = l, j = m + 1;
+        for (int k = l; k <= r; ++k)    // tmp 复制需要排序的子片段,之后进行前后对比,将结果依次存到nums中
+            tmp[k] = nums[k];
+        for (int k = l; k <= r; ++k) {
+            if (i == m + 1)             // 前者已变遍历完,将剩余后者存入nums中
+                nums[k] = tmp[j++];
+            else if (j == r + 1 || tmp[i] <= tmp[j])    // 不存在逆序对,将前者(小)存入nums中
+                nums[k] = tmp[i++];
+            else {                      // 存在逆序对,计数更新,并将后者(小)存入nums中
+                nums[k] = tmp[j++];
+                res += m - i + 1;
+            }
         }
-        for (auto c:keys)       // 遍历出现过的字符(重复也只访问一次),查看该字符是否重复
-            if (hashmap[c])
-                return c;
-        return ' ';
+        return res;
     }
 };
-
-#elif 1
-/// 数组模拟哈希表
-class Solution {
-public:
-    char firstUniqChar(string s) {
-        int count[26] = {0};
-        for (auto c:s)
-            count[c-'a']++;
-
-        for (auto c:s)
-            if (count[c-'a'] == 1)
-                return c;
-
-        return ' ';
-    }
-};
-#endif
 
 int main() {
-    string s = "abaccdeff";
+    vector<int> nums = {7,5,6,4};
     Solution solve;
-    cout << "res = " << solve.firstUniqChar(s) << endl;
+    cout << "res = " << solve.reversePairs(nums) << endl;
 
     cout << "\nFinish";
     return 0;
