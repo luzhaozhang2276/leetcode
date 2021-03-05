76c20925e3f8e999f60c9b1a5f39641a2e1ca2a3
lu <luzhaozhang@foxmail.com>
2021-03-05 20:06:07 +0800

    剑指 Offer 57 - II. 和为s的连续正数序列

--- main.cpp
+++ main.cpp
@@ -1,30 +1,67 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#if 0
+/// 滑动窗口(双指针)
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        int ahead = 0, behind = nums.size() - 1;
+    vector<vector<int>> findContinuousSequence(int target) {
+        int small = 1, big = 2, sum = 3;
+        while (small < big) {
+            if (sum == target) {
+                vector<int> ans;
+                for (int i = small; i <= big; ++i)
+                    ans.push_back(i);
+                res.push_back(ans);
+            }
 
-        while (ahead <= behind) {
-            long sum = nums[ahead] + nums[behind];
-            if (sum == target)
-                return vector<int>{nums[ahead], nums[behind]};
-            else if (sum < target)
-                ahead++;
-            else
-                behind--;
+            if (sum >= target) {
+                sum -= small;
+                ++small;
+            } else {
+                ++big;
+                sum += big;
+            }
         }
-        return vector<int>{};
+        return res;
     }
+
+private:
+    vector<vector<int>> res;
 };
 
+#elif 1
+/// 等差数列求和公式
+class Solution {
+public:
+    vector<vector<int>> findContinuousSequence(int target) {
+        int i = 1;
+        double j = 2.0;
+        vector<vector<int>> res;
+        while (i < j) {
+            j = (-1 + sqrt(1 + 4 * (2 * target + (long) i * i - i))) / 2;
+            if (j == (int)j) {
+                vector<int> ans;
+                for (int k = i; k <= j; ++k)
+                    ans.push_back(k);
+                res.push_back(ans);
+            }
+            ++i;
+        }
+        return res;
+    }
+};
+#endif
+
 int main() {
-    vector<int> nums = {1,2,4,7,11,15};
 
     Solution solve;
-    for (auto p:solve.twoSum(nums, 15))
-        cout << p << ' ';
+    for (auto ex:solve.findContinuousSequence(15)) {
+        for (auto in : ex)
+            cout << in << ' ';
+        cout << endl;
+    }
+
 
     cout << "\nFinish";
     return 0;
