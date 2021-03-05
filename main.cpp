791c0847faae4c863f3b19bbdbbb504518f8c4dd
lu <luzhaozhang@foxmail.com>
2021-03-05 19:22:58 +0800

    剑指 Offer 57. 和为s的两个数字

--- main.cpp
+++ main.cpp
@@ -1,70 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#if 1
-/// 哈希表
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
-        unordered_map<int, int> hashmap;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int ahead = 0, behind = nums.size() - 1;
 
-        for (int i : nums)
-            ++hashmap[i];
-
-        unordered_map<int, int>::const_iterator itr;
-        for (int i : nums) {
-            itr = hashmap.find(i);
-            if (itr->second == 1)
-                return itr->first;
+        while (ahead <= behind) {
+            long sum = nums[ahead] + nums[behind];
+            if (sum == target)
+                return vector<int>{nums[ahead], nums[behind]};
+            else if (sum < target)
+                ahead++;
+            else
+                behind--;
         }
-
-        return -1;
+        return vector<int>{};
     }
 };
-
-#elif 1
-/// 有限状态自动机
-class Solution {
-public:
-    int singleNumber(vector<int>& nums) {
-        int high = 0, low = 0;
-        for (int num : nums) {
-            low  = low ^ num & ~high;   // 直接对32位都进行了运算
-            high = high ^ num & ~low;
-        }
-        return low;
-    }
-};
-
-#elif 1
-/// 遍历统计
-class Solution {
-public:
-    int singleNumber(vector<int>& nums) {
-        int counts[32] = {0};
-        for (auto num:nums) {
-            for (int j = 0; j < 32; ++j) {
-                counts[j] += num & 1;   // 更新第 j 位 (判断最低位，下一步右移更新)
-                num >>= 1;              // j -> j+1
-            }
-        }
-
-        int res = 0, m = 3;
-        for (int i = 0; i < 32; ++i) {
-            res <<= 1;
-            res |= counts[31 - i] % m;
-        }
-
-        return res;
-    }
-};
-#endif
 
 int main() {
-    vector<int> nums = {3,4,3,3};
+    vector<int> nums = {1,2,4,7,11,15};
 
     Solution solve;
-    cout << "res = " << solve.singleNumber(nums) << endl;
+    for (auto p:solve.twoSum(nums, 15))
+        cout << p << ' ';
 
     cout << "\nFinish";
     return 0;
