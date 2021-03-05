723749126ca29889ed8b1a3565f6fcf90db32194
lu <luzhaozhang@foxmail.com>
2021-03-05 21:50:02 +0800

    剑指 Offer 58 - II. 左旋转字符串

--- main.cpp
+++ main.cpp
@@ -1,34 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/// 双指针
+#if 0
+/// 字符串切片
 class Solution {
 public:
-    string reverseWords(string s) {
-        // 删除首尾空格
-        s.erase(s.find_last_not_of(' ') + 1);
-        s.erase(0, s.find_first_not_of(' '));
-
-        int j = s.length() - 1, i = j;
-        string res;
-        while (i >= 0) {
-            while (i >= 0 && s[i] != ' ')
-                --i;
-            res += s.substr(i + 1, j - i) + ' ';
-            while (i >= 0 && s[i] == ' ')
-                --i;
-            j = i;
-        }
-        return res.substr(0, res.size() - 1);
+    string reverseLeftWords(string s, int n) {
+        return s.substr(n, s.length()) + s.substr(0, n);
     }
 };
 
+#elif 0
+/// 字符串遍历拼接
+class Solution {
+public:
+    string reverseLeftWords(string s, int n) {
+        string res;
+        for (int i = n; i < s.length(); ++i)
+            res += s[i];
+        for (int i = 0; i < n; ++i)
+            res += s[i];
+        return res;
+    }
+};
+
+#elif 1
+/// 三次翻转
+class Solution {
+public:
+    string reverseLeftWords(string s, int n) {
+        reverse_string(s, 0, s.length() - 1);
+        reverse_string(s, 0, s.length() - n - 1);
+        reverse_string(s, s.length() - n, s.length() - 1);
+        return s;
+    }
+
+private:
+    void reverse_string(string &s, int start, int end) {
+        for (int i = start; i <= (start + end) / 2; ++i) {
+            char tmp = s[i];
+            s[i] = s[start + end - i];
+            s[start + end - i] = tmp;
+        }
+    }
+};
+
+#endif
 
 int main() {
-    string str = "the sky is blue";
+    string str = "abcdefg";
 
     Solution solve;
-    cout << solve.reverseWords(str) << endl;
+    cout << solve.reverseLeftWords(str, 2) << endl;
 
 
     cout << "\nFinish";
