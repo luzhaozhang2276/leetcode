64f80f084f3a6188dfc6b6427c605419383594af
lu <luzhaozhang@foxmail.com>
2021-03-06 10:39:27 +0800

    剑指 Offer 59 - II. 队列的最大值

--- main.cpp
+++ main.cpp
@@ -1,39 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/// 单调队列
-class Solution {
+class MaxQueue {
+    queue<int> que;
+    deque<int> deq;
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        vector<int> res;
+    MaxQueue() {
 
-        if (nums.empty() || k == 0)
-            return res;
+    }
 
-        deque<int> maxWindow;
-        for (int j = 0, i = 1 - k; j < nums.size(); ++i, ++j) {
-            if (i > 0 && maxWindow.front() == nums[i - 1])  // 最大值超出窗口,则删除队列中的最大值(队首出队)
-                maxWindow.pop_front();
+    int max_value() {
+        return deq.empty() ? -1 : deq.front();
+    }
 
-            while (!maxWindow.empty() && maxWindow.back() < nums[j])    // 保持 deque 单调递减
-                maxWindow.pop_back();
-            maxWindow.push_back(nums[j]);
+    void push_back(int value) {
+        que.push(value);
+        while (!deq.empty() && deq.back() < value)
+            deq.pop_back();
+        deq.push_back(value);
+    }
 
-            if (i >= 0)     // 记录窗口的最大值
-                res.push_back(maxWindow.front());
-        }
-
-        return res;
+    int pop_front() {
+        if (que.empty())
+            return -1;
+        if (deq.front() == que.front())
+            deq.pop_front();
+        int val = que.front();
+        que.pop();
+        return val;
     }
 };
 
 int main() {
-    vector<int> nums = {1,3,-1,-3,5,3,4,7};
-
-    Solution solve;
-    for (auto p : solve.maxSlidingWindow(nums, 3))
-        cout << p << ' ';
-
+    auto *obj = new MaxQueue;
+    obj->push_back(1);
+    obj->push_back(2);
+    cout << obj->max_value() << endl;
+    cout << obj->pop_front() << endl;
+    cout << obj->max_value() << endl;
 
     cout << "\nFinish";
     return 0;
