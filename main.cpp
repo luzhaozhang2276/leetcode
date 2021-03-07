b6285cd676e20279f289d138e0beb5232369abd5
lu <luzhaozhang@foxmail.com>
2021-03-07 10:11:43 +0800

    剑指 Offer 64. 求1+2+…+n

--- main.cpp
+++ main.cpp
@@ -1,23 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/// 递归 (与运算作为递归终止条件)
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int cost = INT_MAX, maxdif = 0;
-        for (auto price : prices) {
-            cost = min(cost, price);
-            maxdif = max(maxdif, price - cost);
-        }
-        return maxdif;
+    int sumNums(int n) {
+        n  && (n += sumNums(n - 1));
+        return n;
     }
 };
 
 int main() {
-    vector<int> prices = {7,1,5,3,6,4};
-
     Solution solve;
-    cout << "profit = " << solve.maxProfit(prices) << endl;
+    cout << "sum = " << solve.sumNums(3) << endl;
 
     cout << "\nFinish";
     return 0;
