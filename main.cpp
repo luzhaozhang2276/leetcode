704803ed00780f28bd47354114d21d244432a792
lu <luzhaozhang@foxmail.com>
2021-03-07 15:11:02 +0800

    剑指 Offer 67. 把字符串转换成整数

--- main.cpp
+++ main.cpp
@@ -1,60 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#if 0
-/// 表格分区 : 双循环 依次遍历下三角和上三角
 class Solution {
 public:
-    vector<int> constructArr(vector<int>& a) {
-        vector<int> b(a.size(), 1);
-//        if (a.empty())
-//            return b;
-//
-//        b[0] = 1;
-        for (int i = 1; i < a.size(); ++i)      // 计算下三角
-            b[i] = b[i - 1] * a[i - 1];
+    int strToInt(string str) {
+        str.erase(0, str.find_first_not_of(' '));
+        if (str.empty())
+            return 0;
 
-        int tmp = 1;        // 计算上三角
-        for (int i = a.size() - 2; i >= 0; --i) {
-            tmp *= a[i + 1];
-            b[i] *= tmp;
+        int i = 1, sign = 1;
+        if (str[0] == '-')
+            sign = -1;
+        else if (str[0] != '+')
+            i = 0;
+
+        int res = 0, boundary = INT_MAX / 10;
+        for (int j = i; j < str.length(); ++j) {
+            if (str[j] < '0' || str[j] > '9')
+                break;
+
+            if (res > boundary || res == boundary && str[j] > '7')
+                return sign == 1 ? INT_MAX : INT_MIN;
+
+            res = 10 * res + (str[j] - '0');
         }
 
-        return b;
+        return sign * res;
     }
 };
-
-#elif 1
-/// 改进: 一次循环   两端同时进行
-class Solution {
-public:
-    vector<int> constructArr(vector<int>& a) {
-        int len = a.size();
-        vector<int> b(len, 1);
-        if (a.empty())
-            return b;
-
-        int left = 1, right = 1;
-        for (int i = 0; i < len; ++i) {
-            b[i] *= left;
-            left *= a[i];               // 计算左边乘积
-
-            b[len - i - 1] *= right;
-            right *= a[len - i - 1];    // 计算右边乘积
-        }
-        return b;
-    }
-};
-
-#endif
 
 
 int main() {
-    vector<int> nums = {1,2,3,4,5};
+    string str = "  -42c";
 
     Solution solve;
-    for (auto sum : solve.constructArr(nums))
-        cout << sum << ' ';
+    cout << "res = " << solve.strToInt(str) << endl;
 
     cout << "\nFinish";
     return 0;
