db9b26315d0b2d0c0563660b7e0ed71efdac59f2
lu <luzhaozhang@foxmail.com>
2021-03-16 17:09:06 +0800

    16. 最接近的三数之和

--- main.cpp
+++ main.cpp
@@ -1,32 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/// 14. 最长公共前缀
-// 纵向查找
+/// 16. 最接近的三数之和
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        string res;
-        if (strs.empty())
-            return res;
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
 
-        for (int i = 0; i < strs[0].size(); ++i) {
-            char tmp = strs[0][i];
-            for (int j = 1; j < strs.size(); ++j) {
-                if (strs[j].size() < i || strs[j][i] != strs[0][i])
-                    return res;
+        int diff = INT_MAX;
+        for (int k = 0; k < nums.size() - 2; ++k) {
+            if (k > 0 && nums[k] == nums[k - 1])    // 剪枝 : 重复
+                continue;
+            int left = k + 1, right = nums.size() - 1;
+            while (left < right) {
+                int tmp = nums[k] + nums[left] + nums[right] - target;
+                diff = (abs(diff) > abs(tmp)) ? tmp : diff;
+
+                if (tmp < 0)
+                    while (left < right && nums[left] == nums[++left]);
+                else if (tmp > 0)
+                    while (left < right && nums[right] == nums[--right]);
+                else
+                    return target;
             }
-            res += tmp;
         }
-        return res;
+
+        return target + diff;
     }
 };
 
 int main() {
-    vector<string> strs = {"flower","dlow","flight"};
+    vector<int> nums = {0,0,0};
 
     Solution solve;
-    cout << "res = " << solve.longestCommonPrefix(strs) << endl;
+    cout << "res = " << solve.threeSumClosest(nums, 1) << endl;
 
     cout << "\nFinish";
     return 0;
