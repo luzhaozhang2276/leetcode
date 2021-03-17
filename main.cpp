d3fcf01fd8836f0fab5ac862d59a5639bd47aaae
lu <luzhaozhang@foxmail.com>
2021-03-17 10:20:35 +0800

    18. 四数之和

--- main.cpp
+++ main.cpp
@@ -1,45 +1,66 @@
 #include <bits/stdc++.h>
 
-#include <utility>
 using namespace std;
 
-/// 17. 电话号码的字母组合
-// 回溯法 (全排列可参照剑指offer38)
+/// 18. 四数之和
+// 双指针 解法与三数之和相同
 class Solution {
 public:
-    vector<string> letterCombinations(string digits) {
-        if (digits.empty())
-            return res;
-
-        phoneMap = {{'2',"abc"}, {'3',"def"}, {'4',"ghi"}, {'5',"jkl"},
-                 {'6',"mno"}, {'7',"pqrs"}, {'8',"tuv"}, {'9',"wxyz"}};
-
-        string combine;
-        traceback(digits, combine, 0);
-        return res;
-    }
-
-private:
-    unordered_map<char, string> phoneMap;
-    vector<string> res;
-    void traceback(string s, string combine, int x) {
-        if (x == s.size()) {
-            res.push_back(combine);       // 添加排列方案
-            return;
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> quadruplets;
+        if (nums.size() < 4) {
+            return quadruplets;
         }
-
-        for (char c : phoneMap[s[x]]) {
-            combine.push_back(c);
-            traceback(s, combine, x+1);          // 开始固定第 x+1 位
-            combine.pop_back();
+        sort(nums.begin(), nums.end());
+        int length = nums.size();
+        for (int i = 0; i < length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) {
+                break;
+            }
+            if (nums[i] + nums[length - 3] + nums[length - 2] + nums[length - 1] < target) {
+                continue;
+            }
+            for (int j = i + 1; j < length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                if (nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) {
+                    break;
+                }
+                if (nums[i] + nums[j] + nums[length - 2] + nums[length - 1] < target) {
+                    continue;
+                }
+                int left = j + 1, right = length - 1;
+                while (left < right) {
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target) {
+                        quadruplets.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        left++;
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
         }
+        return quadruplets;
     }
 };
 
+
 int main() {
     Solution solve;
-    for (auto s : solve.letterCombinations(""))
-        cout << s << ' ';
 
     cout << "\nFinish";
     return 0;
