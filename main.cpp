25dfa38f4473de6994abf1b6ae89023856d54cb5
lu <luzhaozhang@foxmail.com>
2021-03-20 21:25:39 +0800

    NC45. 实现二叉树先序中序后序遍历

--- main.cpp
+++ main.cpp
@@ -1,114 +1,78 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/// NC93. 设计LRU缓存结构
+/// NC45. 实现二叉树先序中序后序遍历
 
-struct DLinkedNode {
-    int key, value;
-    DLinkedNode *prev;
-    DLinkedNode *next;
-    DLinkedNode() : key(0), value(0), prev(nullptr), next(nullptr) {}
-    DLinkedNode(int _key, int _value) : key(_key), value(_value), prev(nullptr), next(nullptr) {}
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int _val) : val(_val), left(nullptr), right(nullptr) {}
 };
 
+
 class Solution {
-    unordered_map<int, DLinkedNode*> cache;
-    DLinkedNode* head;
-    DLinkedNode* tail;
-    int size, capacity;
 public:
-    /**
-     * lru design
-     * @param operators int整型vector<vector<>> the ops
-     * @param k int整型 the k
-     * @return int整型vector
-     */
-    vector<int> LRU(vector<vector<int> >& operators, int k) {
+    vector<vector<int> > threeOrders(TreeNode* root) {
         // write code here
-        if (k < 1)
-            return {};
+        vector<vector<int> > res;
+        preorder(root);
+        middleorder(root);
+        postorder(root);
 
-        size = 0;
-        capacity = k;
-
-        head = new DLinkedNode();
-        tail = new DLinkedNode();
-        head->next = tail;
-        tail->prev = head;
-
-        if (operators.empty())
-            return{};
-
-        vector<int> res;
-        for (auto op : operators) {
-            if (op[0] == 1)
-                put(op[1], op[2]);
-            else if (op[0] == 2)
-                res.push_back(get(op[1]));
-        }
-
+        res.push_back(pre);
+        res.push_back(middle);
+        res.push_back(post);
         return res;
     }
 
-    int get(int _key) {
-        if (!cache.count(_key))     // 不存在
-            return -1;
-        else {      // 存在
-            moveToHead(cache[_key]);
-            return cache[_key]->value;
-        }
-    }
-
-    void put(int _key, int _value) {
-        if (!cache.count(_key)) {   // 不存在
-            auto* ptr = new DLinkedNode(_key, _value);
-            addToHead(ptr);
-            cache[_key] = ptr;
-            ++size;
-            if (size > capacity) {
-                DLinkedNode* removed = removeTail();
-                cache.erase(removed->key);
-                delete removed;
-                --size;
-            }
-        }
-        else {      // 存在
-            cache[_key]->value = _value;
-            moveToHead(cache[_key]);
-        }
-    }
-
 private:
-    void addToHead(DLinkedNode* node) {
-        auto ptr = head->next;
-        head->next = node;
-        node->prev = head;
-        node->next = ptr;
-        ptr->prev = node;
+    vector<int> pre;
+    vector<int> middle;
+    vector<int> post;
+
+    void preorder(TreeNode* root) {
+        if (root == nullptr)
+            return;
+
+        pre.push_back(root->val);
+        preorder(root->left);
+        preorder(root->right);
     }
 
-    void removeNode(DLinkedNode* node) {
-        node->prev->next = node->next;
-        node->next->prev = node->prev;
+    void middleorder(TreeNode* root) {
+        if (root == nullptr)
+            return;
+
+        middleorder(root->left);
+        middle.push_back(root->val);
+        middleorder(root->right);
     }
 
-    void moveToHead(DLinkedNode* node) {
-        removeNode(node);
-        addToHead(node);
-    }
+    void postorder(TreeNode* root) {
+        if (root == nullptr)
+            return;
 
-    DLinkedNode* removeTail() {
-        DLinkedNode* node = tail->prev;
-        removeNode(node);
-        return node;
+        postorder(root->left);
+        postorder(root->right);
+        post.push_back(root->val);
     }
 };
 
 
-
-
 int main() {
+    TreeNode *root = new TreeNode(2);
+    root->left = new TreeNode(1);
+    root->right = new TreeNode(3);
 
+    Solution solve;
+    for (auto nums : solve.threeOrders(root)) {
+        for (auto num : nums)
+            cout << num << ' ';
+        cout << endl;
+    }
 
     cout << "\nFinish";
     return 0;
