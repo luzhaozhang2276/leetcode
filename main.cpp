fa326bdee2bd61be68f1c44d1581f78f2dd69b7e
lu <luzhaozhang@foxmail.com>
2021-03-20 20:43:38 +0800

    146. LRU 缓存机制

--- main.cpp
+++ main.cpp
@@ -1,32 +1,95 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-/// 26. 删除有序数组中的重复项
-// 双指针 (快慢指针)
-class Solution {
+/// 146. LRU 缓存机制
+
+struct DLinkedNode {
+    int key, value;
+    DLinkedNode *prev;
+    DLinkedNode *next;
+    DLinkedNode() : key(0), value(0), prev(nullptr), next(nullptr) {}
+    DLinkedNode(int _key, int _value) : key(_key), value(_value), prev(nullptr), next(nullptr) {}
+};
+
+class LRUCache {
+    unordered_map<int, DLinkedNode*> cache;
+    DLinkedNode* head;
+    DLinkedNode* tail;
+    int size, capacity;
 public:
-    int removeDuplicates(vector<int>& nums) {
-        if (nums.empty())
-            return 0;
+    LRUCache(int _capacity) : size(0), capacity(_capacity) {
+        head = new DLinkedNode();
+        tail = new DLinkedNode();
+        head->next = tail;
+        tail->prev = head;
+    }
 
-        int i = 0;
-        for (int j = 1; j < nums.size(); ++j)
-            if (nums[j] != nums[i])
-                nums[++i] = nums[j];
+    int get(int _key) {
+        if (!cache.count(_key))     // 不存在
+            return -1;
+        else {      // 存在
+            moveToHead(cache[_key]);
+            return cache[_key]->value;
+        }
+    }
 
-        return i + 1;
+    void put(int _key, int _value) {
+        if (!cache.count(_key)) {   // 不存在
+            auto* ptr = new DLinkedNode(_key, _value);
+            addToHead(ptr);
+            cache[_key] = ptr;
+            ++size;
+            if (size > capacity) {
+                DLinkedNode* removed = removeTail();
+                cache.erase(removed->key);
+                delete removed;
+                --size;
+            }
+        }
+        else {      // 存在
+            cache[_key]->value = _value;
+            moveToHead(cache[_key]);
+        }
+    }
+
+private:
+    void addToHead(DLinkedNode* node) {
+        auto ptr = head->next;
+        head->next = node;
+        node->prev = head;
+        node->next = ptr;
+        ptr->prev = node;
+    }
+
+    void removeNode(DLinkedNode* node) {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+    }
+
+    void moveToHead(DLinkedNode* node) {
+        removeNode(node);
+        addToHead(node);
+    }
+
+    DLinkedNode* removeTail() {
+        DLinkedNode* node = tail->prev;
+        removeNode(node);
+        return node;
     }
 };
 
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
+
+
+
+
 int main() {
-    vector<int> nums = {0,0,1,1,1,2,2,3,3,4};
 
-    Solution solve;
-    cout << "res = " << solve.removeDuplicates(nums) << endl;
-
-    for (auto num : nums)
-        cout << num << ' ';
 
     cout << "\nFinish";
     return 0;
