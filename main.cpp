c4149cb622e480cf36c9dca7565aa8023c280a9a
lu <luzhaozhang@foxmail.com>
2021-03-24 15:32:25 +0800

    1. 两数之和

--- main.cpp
+++ main.cpp
@@ -1,48 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/// 215. 数组中的第K个最大元素
-
-#if 1
-/// 优先队列
-struct cmp {
-    bool operator()(int a, int b) {
-        return a > b;
-    }
-};
+/// 1. 两数之和
 
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-
-        // priority_queue<int, vector<int>, less<int> > heap;       // 大顶堆(默认)
-        // priority_queue<int, vector<int>, greater<int> > heap;    // 小顶堆
-
-
-        priority_queue<int, vector<int>, cmp> heap;   // 默认大顶堆, 此处应为小顶堆
-        for (int i = 0; i < k; ++i)
-            heap.push(nums[i]);
-
-        for (int i = k; i < nums.size(); ++i) {
-            if (nums[i] > heap.top()) {
-                heap.pop();
-                heap.push(nums[i]);
-            }
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_set<int> set;
+        for (auto num : nums) {
+            auto ptr = set.find(target - num);
+            if (ptr != set.end())
+                return {num, *ptr};
+            set.insert(num);
         }
-        return heap.top();
 
+        return {};
     }
 };
-
-#elif 1
-
-#endif
 
 int main() {
     vector<int> nums = {3,6,1,4,2,8,9};
 
     Solution solve;
-    cout << "num = " << solve.findKthLargest(nums, 4);
+    for (auto num : solve.twoSum(nums, 14))
+        cout << num << ' ';
 
     cout << "\nFinish";
     return 0;
