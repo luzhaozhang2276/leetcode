c8d1049ef5766e21ba1272e1923a281465403ad9
lu <luzhaozhang@foxmail.com>
2021-03-24 17:51:22 +0800

    剑指 Offer 42. 连续子数组的最大和

--- main.cpp
+++ main.cpp
@@ -1,27 +1,73 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/// 剑指 Offer 10- I. 斐波那契数列
+/// 剑指 Offer 42. 连续子数组的最大和
 
+#if 0
+// 双指针
 class Solution {
 public:
-    int fib(int n) {
-        if (n <= 1)
-            return n;
+    int maxSubArray(vector<int>& nums) {
+        int curr = INT_MIN, res = INT_MIN;
 
-        int fminuOne = 1, fminuTwo = 0, res;
-        for (int i = 2; i <= n; ++i) {
-            res = (fminuOne + fminuTwo) % 1000000007;
-            fminuTwo = fminuOne;
-            fminuOne = res;
+//        for (int i = 0; i < nums.size(); ++i) {
+//            if (curr < 0)
+//                curr = max(curr, nums[i]);
+//            else
+//                curr += nums[i];
+//
+//            res = max(curr, res);
+//        }
+
+        for (auto num : nums) {
+            curr = (curr < 0) ? max(curr, num) : curr + num;
+            res = max(curr, res);
         }
 
         return res;
     }
 };
+#elif 1
+/// DP 解法
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int res = nums[0];
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i-1] > 0)
+                nums[i] = nums[i-1] + nums[i];
+            res = max(res,nums[i]);
+        }
+        return res;
+    }
+};
+#elif 1
+// DP
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int res = nums[0];
+        vector<int> dp(nums.size(), nums[0]);
+        for (int i = 1; i < nums.size(); ++i) {
+            if (dp[i-1] <= 0)
+                dp[i] = nums[i];
+            else
+                dp[i] = dp[i-1] + nums[i];
+
+            res = max(res, dp[i]);
+
+        }
+
+        return res;
+    }
+};
+#endif
+
 
 int main() {
+    vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4};
     Solution solve;
+    cout << "res = " << solve.maxSubArray(nums) << endl;
 
     cout << "\nFinish";
     return 0;
