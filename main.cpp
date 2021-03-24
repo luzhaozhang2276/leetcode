67a5570550b1966554f9892f9b9baef7d93f51ad
lu <luzhaozhang@foxmail.com>
2021-03-24 15:14:46 +0800

    215. 数组中的第K个最大元素

--- main.cpp
+++ main.cpp
@@ -1,33 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
+/// 215. 数组中的第K个最大元素
 
-/// 94. 二叉树的中序遍历
-class Solution {
-    vector<int> res;
-public:
-    vector<int> inorderTraversal(TreeNode* root) {
-        if (root == nullptr)
-            return {};
-
-        inorderTraversal(root->left);
-        res.push_back(root->val);
-        inorderTraversal(root->right);
-
-        return res;
+#if 1
+/// 优先队列
+struct cmp {
+    bool operator()(int a, int b) {
+        return a > b;
     }
 };
 
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+
+        // priority_queue<int, vector<int>, less<int> > heap;       // 大顶堆(默认)
+        // priority_queue<int, vector<int>, greater<int> > heap;    // 小顶堆
+
+
+        priority_queue<int, vector<int>, cmp> heap;   // 默认大顶堆, 此处应为小顶堆
+        for (int i = 0; i < k; ++i)
+            heap.push(nums[i]);
+
+        for (int i = k; i < nums.size(); ++i) {
+            if (nums[i] > heap.top()) {
+                heap.pop();
+                heap.push(nums[i]);
+            }
+        }
+        return heap.top();
+
+    }
+};
+
+#elif 1
+
+#endif
+
 int main() {
+    vector<int> nums = {3,6,1,4,2,8,9};
+
     Solution solve;
+    cout << "num = " << solve.findKthLargest(nums, 4);
 
     cout << "\nFinish";
     return 0;
