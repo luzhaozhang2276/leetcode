b89cb082cdbf020f4900313b565e850d98b75513
lu <luzhaozhang@foxmail.com>
2021-03-28 09:59:25 +0800

    剑指 Offer 68 - II. 二叉树的最近公共祖先

--- main.cpp
+++ main.cpp
@@ -1,22 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/// 剑指 Offer 48. 最长不含重复字符的子字符串
+/// 剑指 Offer 68 - II. 二叉树的最近公共祖先
 
-/// 双指针
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
+/// 递归
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        unordered_map<char, int> hash;
-        int i = -1, res = 0;
-        for (int j = 0; j < s.size(); ++j) {
-            if (hash.find(s[j]) != hash.end())    // 重复, 更新左指针
-                i = max(hash[s[j]], i);
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        // 终止条件: 叶子节点 or 找到 p q
+        if (root == nullptr || root == p || root == q)
+            return root;
 
-            hash[s[j]] = j;
-            res = max(res, j-i);
-        }
-        return res;
+        // 递归调用左右节点
+        auto left = lowestCommonAncestor(root->left, p, q);
+        auto right = lowestCommonAncestor(root->right, p, q);
+
+        // if (left == nullptr && right == nullptr)         // 均为空
+        //     return nullptr;
+        if (left == nullptr)        // 左空右不空
+            return right;
+        if (right == nullptr)   // 左非空右空
+            return left;
+
+        return root;                // 均非空, 即p = q = root
     }
 };
 
