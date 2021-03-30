23177bdeac617d28c9d7245c61de1397f150721b
luzhaozhang2276 <luzhaozhang@foxmail.com>
2021-03-30 14:22:02 +0800

    20. 有效的括号

--- main.cpp
+++ main.cpp
@@ -1,37 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/// 695. 岛屿的最大面积
-
-/// DFS
-// 外层:遍历表格,对‘1'进行DFS
-// 内层:DFS遍历,对当前层置0
+/// 20. 有效的括号
 class Solution {
 public:
-    int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int maxArea = 0, currArea = 0;
-        for (int i = 0; i < grid.size(); ++i) {
-            for (int j = 0; j < grid[0].size(); ++j) {
-                if (grid[i][j] == 1) {
-                    currArea = dfs(grid, i, j);
-                    maxArea = max(maxArea, currArea);
-                }
+    bool isValid(string s) {
+        unordered_map<char, char> hash = {{'[',']'}, {'(',')'}, {'{','}'}, {'?', '?'}};
+        stack<char> symbol;
+        for (auto c : s) {
+            if (hash.find(c) != hash.end())   // found left
+                symbol.push(c);
+            else {  // found right
+                if (symbol.empty() || hash[symbol.top()] != c)     // unmatched
+                    return false;
+                symbol.pop();
             }
         }
-        return maxArea;
-    }
 
-private:
-    int dfs(vector<vector<int>>& grid, int i, int j) {
-        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == 0)
-            return 0;
-        grid[i][j] = 0;
-
-        return 1 + dfs(grid, i-1, j) + dfs(grid, i+1, j) + dfs(grid, i, j-1) + dfs(grid, i, j+1);
+        return symbol.empty();
     }
 };
-/// BFS
-// 内层使用queue进行BFS遍历
 
 int main() {
     Solution solve;
