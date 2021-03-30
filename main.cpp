736f0a6d945b90d5a9f350a1a04d5ffdf976fc3e
luzhaozhang2276 <luzhaozhang@foxmail.com>
2021-03-30 11:27:21 +0800

    695. 岛屿的最大面积

--- main.cpp
+++ main.cpp
@@ -1,37 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/// 200. 岛屿数量
+/// 695. 岛屿的最大面积
 
 /// DFS
 // 外层:遍历表格,对‘1'进行DFS
 // 内层:DFS遍历,对当前层置0
 class Solution {
 public:
-    int numIslands(vector<vector<char>>& grid) {
-        int count = 0;
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int maxArea = 0, currArea = 0;
         for (int i = 0; i < grid.size(); ++i) {
             for (int j = 0; j < grid[0].size(); ++j) {
-                if (grid[i][j] == '1') {
-                    dfs(grid, i, j);
-                    count++;
+                if (grid[i][j] == 1) {
+                    currArea = dfs(grid, i, j);
+                    maxArea = max(maxArea, currArea);
                 }
             }
         }
-
-        return count;
+        return maxArea;
     }
 
 private:
-    void dfs(vector<vector<char>>& grid, int i, int j) {
-        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == '0')
-            return;
+    int dfs(vector<vector<int>>& grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == 0)
+            return 0;
+        grid[i][j] = 0;
 
-        grid[i][j] = '0';
-        dfs(grid, i-1, j);
-        dfs(grid, i+1, j);
-        dfs(grid, i, j-1);
-        dfs(grid, i, j+1);
+        return 1 + dfs(grid, i-1, j) + dfs(grid, i+1, j) + dfs(grid, i, j-1) + dfs(grid, i, j+1);
     }
 };
 /// BFS
