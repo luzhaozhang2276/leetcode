6dae47c39a60d98695052b595ce90abd0312fb99
luzhaozhang2276 <luzhaozhang@foxmail.com>
2021-07-15 00:03:42 +0800

    34. 在排序数组中查找元素的第一个和最后一个位置

--- main.cpp
+++ main.cpp
@@ -2,42 +2,47 @@
 
 using namespace std;
 
-/// 33. 搜索旋转排序数组
-/// 二分法
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int l = 0, r = nums.size() - 1;
-        int mid;
+    /// 查找第一个比target大的数
+    int findFirst(vector<int>& nums, int target) {
+        if (nums.empty() || target > nums[nums.size()-1])
+            return -1;
+
+        int l = 0, r = nums.size() - 1, mid;
         while (l <= r) {
             mid = (l + r) / 2;
-            if (nums[mid] == target)
-                return mid;
-
-            if (nums[l] <= nums[mid]) {
-                if (nums[l] <= target && target < nums[mid])
-                    r = mid - 1;
-                else
-                    l = mid + 1;
-            }
-            else {
-                if (nums[mid] < target && target <= nums[r])
-                    l = mid + 1;
-                else
-                    r = mid - 1;
-            }
-
+            if (nums[mid] >= target)
+                r = mid - 1;
+            else
+                l = mid + 1;
         }
+        return l;
+    }
 
-        return -1;
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int l = findFirst(nums, target);
+        int r = findFirst(nums, target + 1);
+
+        if (l == -1 || nums[l] != target)   // 超出范围 or 不存在target
+            return {-1, -1};
+        if (r == -1)        // 最后一个数为target
+            r = nums.size();
+
+        return {l, r-1};
     }
 };
 
 int main() {
-    vector<int> nums = {4,5,6,7,0,1,2};
+    vector<int> nums = {5,7,7,8,8,10};
+//    vector<int> nums = {1};
 
     Solution solve;
-    cout << "res = " << solve.search(nums, 8) << endl;
+
+    cout << solve.findFirst(nums, 11) <<endl;
+
+    for (const auto &p : solve.searchRange(nums, 1))
+        cout << p << ' ';
 
 
     cout << "\nFinish";
