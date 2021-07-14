c4918f32b070b4ce14dee8c0603dc7348a6d66b0
luzhaozhang2276 <luzhaozhang@foxmail.com>
2021-07-14 21:17:09 +0800

    33. 搜索旋转排序数组

--- main.cpp
+++ main.cpp
@@ -2,35 +2,43 @@
 
 using namespace std;
 
+/// 33. 搜索旋转排序数组
+/// 二分法
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-        if (nums.size() == 1)
-            return;
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        int mid;
+        while (l <= r) {
+            mid = (l + r) / 2;
+            if (nums[mid] == target)
+                return mid;
 
-        int l = nums.size() - 2;
-        int r = nums.size() - 1;
-        while (l >= 0 && nums[l] >= nums[l+1])  // 从后往前找到首个递减的数
-            --l;
+            if (nums[l] <= nums[mid]) {
+                if (nums[l] <= target && target < nums[mid])
+                    r = mid - 1;
+                else
+                    l = mid + 1;
+            }
+            else {
+                if (nums[mid] < target && target <= nums[r])
+                    l = mid + 1;
+                else
+                    r = mid - 1;
+            }
 
-        if (l >= 0) {
-            while (r >=0 && nums[r] <= nums[l]) // 在l后找到首个大的数
-                --r;
-            swap(nums[l], nums[r]);
         }
 
-        sort(nums.begin() + l + 1, nums.end()); // 后边元素有序(l=-1,则全部重排)
+        return -1;
     }
 };
 
 int main() {
-    vector<int> nums = {1,2,3};
+    vector<int> nums = {4,5,6,7,0,1,2};
 
     Solution solve;
-    solve.nextPermutation(nums);
+    cout << "res = " << solve.search(nums, 8) << endl;
 
-    for (const auto &n : nums)
-        cout << n << ' ';
 
     cout << "\nFinish";
     return 0;
