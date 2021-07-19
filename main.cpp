7b14682c834c2f50000cc37cecb163b79752df8b
luzhaozhang2276 <luzhaozhang@foxmail.com>
2021-07-19 21:32:26 +0800

    53. 最大子序和

--- main.cpp
+++ main.cpp
@@ -2,44 +2,25 @@
 
 using namespace std;
 
-/// 39. 组合总和
-/// 回溯+剪枝
 class Solution {
-    vector<vector<int>> res;
-    vector<int> combine;
-
-    void dfs(vector<int>& candidates, int target, int idx) {
-        if (idx == candidates.size()) {
-            if (target == 0)
-                res.push_back(combine);
-            return;
-        }
-
-        dfs(candidates, target, idx+1);
-
-        if (target - candidates[idx] >= 0) {
-            combine.push_back(candidates[idx]);
-            dfs(candidates, target-candidates[idx], idx);   // 可重复 idx， 不可重复 idx+1
-            combine.pop_back();
-        }
-    }
-
 public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        dfs(candidates, target, 0);
+    int maxSubArray(vector<int>& nums) {
+        int res = nums[0], pre = nums[0];
+        for (int i = 1; i < nums.size(); ++i) {
+            pre = max(pre+nums[i], nums[i]);
+            res = max(res, pre);
+        }
+
         return res;
     }
 };
 
 int main() {
-    vector<int> nums = {2, 3, 5, 7};
+    vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4};
 
     Solution solve;
-    for (const auto &ps :solve.combinationSum(nums, 7)) {
-        for (const auto &num : ps)
-            cout << num << ' ';
-        cout <<endl;
-    }
+    cout << "res = " << solve.maxSubArray(nums) <<endl;
+
 
     cout << "\nFinish";
     return 0;
