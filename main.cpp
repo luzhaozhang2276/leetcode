fe0829d299d2967dfef208de7e8e64a3c466540d
luzhaozhang2276 <luzhaozhang@foxmail.com>
2021-07-21 23:50:24 +0800

    64. 最小路径和

--- main.cpp
+++ main.cpp
@@ -2,24 +2,28 @@
 
 using namespace std;
 
-/// 62. 不同路径  二维DP
+/// 64. 最小路径和  二维DP
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, 1));
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+
+        for (int i = 1; i < n; ++i)
+            grid[0][i] += grid[0][i-1];
+        for (int j = 1; j < m; ++j)
+            grid[j][0] += grid[j-1][0];
         for (int i = 1; i < m; ++i) {
             for (int j = 1; j < n; ++j) {
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                grid[i][j] += min(grid[i-1][j], grid[i][j-1]);
             }
         }
 
-        return dp[m-1][n-1];
+        return grid[m-1][n-1];
     }
 };
 
 int main() {
     Solution solve;
-    cout << "res = " << solve.uniquePaths(7, 3) <<endl;
 
     cout << "\nFinish";
     return 0;
