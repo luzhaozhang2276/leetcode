8dfea8b3e45c1c81dda752d4b43a319719b06266
luzhaozhang2276 <luzhaozhang@foxmail.com>
2021-07-21 23:31:22 +0800

    56. 合并区间

--- main.cpp
+++ main.cpp
@@ -2,30 +2,34 @@
 
 using namespace std;
 
-/// 55. 跳跃游戏
+/// 56. 合并区间
 class Solution {
 public:
-    bool canJump(vector<int>& nums) {
-        int end = 0;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (end < i)
-                return false;
-            end = max(end, i + nums[i]);
-            if (end >= nums.size() - 1)
-                return true;
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> res;
+        res.push_back(intervals[0]);
+        for (int i = 1; i < intervals.size(); ++i) {
+            int L = intervals[i][0], R = intervals[i][1];
+            if (L <= res.back()[1])
+                res.back()[1] = max(res.back()[1], R);
+            else
+                res.push_back(intervals[i]);
         }
 
-        return end >= nums.size();
+        return res;
     }
 };
 
 int main() {
-    vector<int> nums = {3,2,1,0,4};
+    vector<vector<int>> intervals;
+    // intervals = {{1,3}, {2,6}, {8,10}, {15,18}};
+    intervals = {{1,3}, {8,10}, {2,6}, {15,18}};
 
     Solution solve;
-    string res;
-    res = solve.canJump(nums) ? "true" : "false";
-    cout << "res = " << res << endl;
+    for (const auto &nums : solve.merge(intervals))
+        cout << nums[0] << ',' << nums[1] << endl;
 
     cout << "\nFinish";
     return 0;
