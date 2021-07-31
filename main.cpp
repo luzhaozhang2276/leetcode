57a9b893ce8b0269dc04483a505740b90f9d2e28
luzhaozhang2276 <luzhaozhang@foxmail.com>
2021-08-01 00:03:06 +0800

    76. 最小覆盖子串

--- main.cpp
+++ main.cpp
@@ -2,33 +2,59 @@
 
 using namespace std;
 
-/// 75. 颜色分类  双指针
+/// 76. 最小覆盖子串
+// 滑动窗口 + 哈希表
 class Solution {
-public:
-    void sortColors(vector<int>& nums) {
-        int p0 = 0, p2 = nums.size() - 1;
-        for (int i = 0; i <= p2; ++i) {
-            while (i <= p2 && nums[i] == 2) {
-                swap(nums[i], nums[p2]);
-                p2--;
-            }
+    unordered_map<char, int> win, ori;
 
-            if (nums[i] == 0) {
-                swap(nums[i], nums[p0]);
-                p0++;
+    bool check() {
+        for (const auto &p : ori) {
+            if (win[p.first] < p.second)
+                return false;
+        }
+        return true;
+    }
+
+public:
+    string minWindow(string s, string t) {
+        // initial
+        for (const auto &p : t)
+            ++ori[p];
+
+        int l = 0, r = -1, start = -1, len = INT_MAX;
+        int size = s.size();
+        while (r < size) {
+            // 右移
+            if (ori.find(s[++r]) != ori.end())
+                ++win[s[r]];
+
+            // 左移
+            while (l <= r && check()) {
+                // update
+                if (r - l + 1 < len) {
+                    len = r - l + 1;
+                    start = l;
+                }
+
+                // remove l in win
+                if (ori.find(s[l]) != ori.end())
+                    --win[s[l]];
+
+                ++l;
             }
         }
+
+        return start == -1 ? string(): s.substr(start, len);
     }
 };
 
+
 int main() {
-    vector<int> nums = {2,0,2,1,1,0};
-    nums = {1,0,1};
+    string s = "cabwefgewcwaefgcf";
+    string t = "cae";
 
     Solution solve;
-    solve.sortColors(nums);
-    for (const auto n : nums)
-        cout << n << ' ';
+    cout <<"res = " << solve.minWindow(s, t) << endl;
 
     cout << "\nFinish";
     return 0;
