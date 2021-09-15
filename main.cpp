b7d7744bf7ec0241b2da02cf2030de9165436a36
lu <luzhaozhang@foxmail.com>
2021-09-15 10:36:43 +0800

    华为拓扑排序

--- main.cpp
+++ main.cpp
@@ -1,100 +1,90 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#if 1
-/// 含手续费
-class Solution {
-public:
-    int maxProfit(vector<int>& prices, int fee = 0) {
-        int n = prices.size();
-        int sell = 0, buy = -prices[0];
-        for (int i = 1; i < n; ++i) {
-            sell = max(sell, buy + prices[i] - fee);
-            buy = max(buy, sell - prices[i]);
-        }
-        return sell;
+vector<string> simpleSplit(const string &str, char a)
+{
+    std::vector<std::string> vec;
+    std::stringstream ss;
+    ss.str(str);
+    std::string s;
+    while (std::getline(ss, s, a))
+    {
+        vec.push_back(s);
     }
+    return vec;
+}
+
+struct Code
+{
+    // string name;
+    int degree;
+    int time;
+    vector<string> adj;
 };
 
-#elif 1
+int course(vector<string> & data) {
+    unordered_map<string, Code> hash;
 
-/// 含冷冻期
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        if (prices.empty()) {
-            return 0;
-        }
-
-        int n = prices.size();
-        // f[i][0]: 手上持有股票的最大收益
-        // f[i][1]: 手上不持有股票，并且处于冷冻期中的累计最大收益
-        // f[i][2]: 手上不持有股票，并且不在冷冻期中的累计最大收益
-        vector<vector<int>> f(n, vector<int>(3));
-        f[0][0] = -prices[0];
-        for (int i = 1; i < n; ++i) {
-            f[i][0] = max(f[i - 1][0], f[i - 1][2] - prices[i]);
-            f[i][1] = f[i - 1][0] + prices[i];
-            f[i][2] = max(f[i - 1][1], f[i - 1][2]);
-        }
-        return max(f[n - 1][1], f[n - 1][2]);
+    for (int i = 1; i < data.size(); ++i) {
+        auto s = simpleSplit(data[i], ',');
+        Code tar;
+        tar.time = stoi(s[1]);
+        tar.degree = s.size() - 2;
+        hash[s[0]] = tar;
     }
-};
 
-#elif 1
+    for (int i = 1; i < data.size(); ++i) {
+        auto s = simpleSplit(data[i], ',');
+        for (int j = 2; j < s.size(); ++j) {
+            if (hash.find(s[j]) == hash.end())
+                return -1;
+            hash[s[j]].adj.emplace_back(s[0]);
+        }
+    }
 
-/// 固定次数买卖
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        int k = 2;
-        vector<int> buy(k, -prices[0]);
-        vector<int> sell(k, 0);
-        for (int i = 1; i < prices.size(); ++i) {
-            buy[0] = max(buy[0], -prices[i]);
-            sell[0] = max(sell[0], buy[0] + prices[i]);
-            for (int j = 1; j < k; ++j) {
-                buy[j] = max(buy[j], sell[j-1]-prices[i]);
-                sell[j] = max(sell[j], buy[j] + prices[i]);
+    queue<string> Q;
+    for (const auto &p : hash) {
+        if (p.second.degree == 0)
+            Q.emplace(p.first);
+    }
+
+    int res = 0;
+    while (!Q.empty()) {
+        int curr_size = Q.size();
+        int curr_time = 0;
+        for (int i = 0; i < curr_size; ++i) {
+            string top = Q.front();
+            curr_time = max(curr_time, hash[top].time);
+            Q.pop();
+            for (const auto &p : hash[top].adj) {
+                hash[p].degree--;
+                if (hash[p].degree == 0)
+                    Q.emplace(p);
             }
         }
-        return sell[k-1];
+        res += curr_time;
+        if (hash[data[0]].degree == 0)
+            return res + hash[data[0]].time;
     }
-};
-#elif 1
-/// 多次买卖
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        int profit = 0;
-        for (int i = 1; i < prices.size(); ++i) {
-            int curr = prices[i] - prices[i-1];
-            if (curr > 0)
-                profit += curr;
-        }
-        return profit;
-    }
-};
-#elif 1
-/// 单次买卖
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        int cost = INT_MAX, maxdif = 0;
-        for (auto price : prices) {
-            cost = min(cost, price);
-            maxdif = max(maxdif, price - cost);
-        }
-        return maxdif;
-    }
-};
-#endif
+
+    if (hash[data[0]].degree != 0)
+        return -1;
+
+    return res == 0 ? -1 : res;
+}
 
 int main() {
-    vector<int> prices = {7,1,5,3,6,4};
 
-    Solution solve;
-    cout << solve.maxProfit(prices) << endl;
+    vector<string> data;
+    data.emplace_back("module3");
+    data.emplace_back("module1,10");
+//    data.emplace_back("module2,5,module1");
+    data.emplace_back("module2,5");
+    data.emplace_back("module3,10,module1,module2");
+//    data.emplace_back("module4,10,module3");
+
+
+    cout << course(data) << endl;
 
     return 0;
 }
