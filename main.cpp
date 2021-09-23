e834b08e29ddcf945c620ed8ec45a1e91424fa83
luzhoazhang <luzhaozhang@foxmail.com>
2021-09-23 16:33:42 +0800

    华为：矩阵中的路径

--- main.cpp
+++ main.cpp
@@ -1,90 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<string> simpleSplit(const string &str, char a)
+class Solution
 {
-    std::vector<std::string> vec;
-    std::stringstream ss;
-    ss.str(str);
-    std::string s;
-    while (std::getline(ss, s, a))
-    {
-        vec.push_back(s);
+    int row_;
+    int col_;
+    int x_;
+    int y_;
+    int path_;
+    inline bool inMatrix(int i, int j) const {
+        return !(i < 0 || i >= row_ || j < 0 || j >= col_);
     }
-    return vec;
-}
 
-struct Code
-{
-    // string name;
-    int degree;
-    int time;
-    vector<string> adj;
+    bool dfs(vector<vector<int>> &matrix, int i, int j) {
+        if (i == x_ && j == y_)
+            return true;
+
+        if (!inMatrix(i, j) || matrix[i][j] != 1)
+            return false;
+
+        // 访问标记
+        matrix[i][j] = 0;
+        path_++;
+
+        if (dfs(matrix, i, j - 1) ||
+            dfs(matrix, i, j + 1) ||
+            dfs(matrix, i - 1, j) ||
+            dfs(matrix, i + 1, j))
+            return true;
+
+        // 回退
+        matrix[i][j] = 1;
+        path_--;
+
+        return false;
+    }
+
+public:
+    int solve(vector<vector<int>> &matrix, int a, int b, int x, int y)
+    {
+        row_ = matrix.size();
+        col_ = matrix[0].size();
+        x_ = x;
+        y_ = y;
+        path_ = 0;
+
+        if (matrix[a][b] == 0 || matrix[x][y] == 0)
+            return -1;
+
+        if (dfs(matrix, a, b))
+            return path_;
+
+        return -1;
+    }
 };
 
-int course(vector<string> & data) {
-    unordered_map<string, Code> hash;
-
-    for (int i = 1; i < data.size(); ++i) {
-        auto s = simpleSplit(data[i], ',');
-        Code tar;
-        tar.time = stoi(s[1]);
-        tar.degree = s.size() - 2;
-        hash[s[0]] = tar;
-    }
-
-    for (int i = 1; i < data.size(); ++i) {
-        auto s = simpleSplit(data[i], ',');
-        for (int j = 2; j < s.size(); ++j) {
-            if (hash.find(s[j]) == hash.end())
-                return -1;
-            hash[s[j]].adj.emplace_back(s[0]);
-        }
-    }
-
-    queue<string> Q;
-    for (const auto &p : hash) {
-        if (p.second.degree == 0)
-            Q.emplace(p.first);
-    }
-
-    int res = 0;
-    while (!Q.empty()) {
-        int curr_size = Q.size();
-        int curr_time = 0;
-        for (int i = 0; i < curr_size; ++i) {
-            string top = Q.front();
-            curr_time = max(curr_time, hash[top].time);
-            Q.pop();
-            for (const auto &p : hash[top].adj) {
-                hash[p].degree--;
-                if (hash[p].degree == 0)
-                    Q.emplace(p);
-            }
-        }
-        res += curr_time;
-        if (hash[data[0]].degree == 0)
-            return res + hash[data[0]].time;
-    }
-
-    if (hash[data[0]].degree != 0)
-        return -1;
-
-    return res == 0 ? -1 : res;
-}
-
 int main() {
+    vector<vector<int>> matrix = {{0, 1, 0, 0},
+                                  {1, 1, 0, 0},
+                                  {0, 1, 1, 0},
+                                  {0, 0, 1, 1}};
 
-    vector<string> data;
-    data.emplace_back("module3");
-    data.emplace_back("module1,10");
-//    data.emplace_back("module2,5,module1");
-    data.emplace_back("module2,5");
-    data.emplace_back("module3,10,module1,module2");
-//    data.emplace_back("module4,10,module3");
+    Solution ans;
+    cout << ans.solve(matrix, 3, 3, 1, 1) << endl;
 
-
-    cout << course(data) << endl;
 
     return 0;
 }
